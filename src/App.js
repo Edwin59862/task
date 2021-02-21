5153a2f27ef8d33cb768671ab97fb118b8c92afc
Edwin59862 <70116838+Edwin59862@users.noreply.github.com>
2021-02-21 18:32:30 -0500

    create new project task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {isValidElement, useState} from 'react'
+import { isEmpty,size } from 'lodash'
+import shortid from 'shortid'
 
 function App() {
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+  const [editMode, setEditMode] = useState(false)
+  const [id,setId] = useState('')
+  const [error, setError] = useState(null)
+
+  const validForm = () => {
+    let isValid = true
+    setError(null)
+    if (isEmpty(task)) {
+      setError('Debes ingresar una tarea')
+      isValid = false
+    }
+    return isValid
+  }
+
+  //Agregar tarea
+  const addTask = (e) => {
+    e.preventDefault()
+    if(!validForm()) {
+      return
+    }
+    const newTask = {
+      id: shortid.generate(),
+      name: task
+    }
+    
+    setTasks([...tasks,newTask])
+    setTask("")
+  }
+
+  //Eliminar tarea
+  const deleteTask = (id) => {
+    const filteredTask = tasks.filter(task => task.id !== id)
+    setTasks(filteredTask)
+  }
+  
+  //Saber si estamos editando la tarea
+  const editTask =(theTask)=>{
+    setTask(theTask.name)
+    setEditMode(true)
+    setId(theTask.id)    
+  }
+  
+  //Salvar tarea editada
+  const saveTask = (e) => {
+    e.preventDefault()
+    if(!validForm()) {
+      return
+    }
+
+    const editedTask = tasks.map(item => item.id === id ? { id, name:task} : item)
+    setTasks(editedTask)
+    setEditMode(false)
+    setTask('')
+    setId('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-5' >
+      <h1>Tareas</h1>
+      <hr/>
+      <div className='row'>
+        <div className='col-8'>
+          <h4 className='text-center'>Lita de tareas</h4>
+          {
+            size(tasks) === 0 ? <li className='list-group-item'>Aún no hay tareas programadas</li> : (
+              <ul className='list-group'>
+                {
+                  tasks.map((task) => (
+                    <li className='list-group-item' key={task.id}>
+                      <span className='lead'>{task.name}</span>
+                      <button 
+                        className='btn btn-danger btn-sm float-right mx-2'
+                        onClick = {() => deleteTask(task.id)}
+                      >
+                          Eliminar
+                      </button>
+                      <button 
+                        className='btn btn-warning btn-sm float-right'
+                        onClick = {() => editTask(task)}
+                      >
+                          Editar
+                      </button>
+                    </li>
+                  ))              
+                }
+              </ul>
+            )            
+          }          
+        </div>
+        <div className='col-4'>
+          <h4 className='text-center'>
+            { editMode ? 'Modificar tarea': 'Agregar tarea'}            
+          </h4>
+          { error && <span className='text-danger'>{error}</span>}
+          <form onSubmit={editMode ? saveTask : addTask}>
+            <input
+              type='text'
+              className='form-control mb-2'
+              placeholder='Ingrese la tarea...'
+              onChange={(text)=> setTask(text.target.value)}
+              value={task}            
+            />            
+            <button 
+              className={ editMode ?'btn btn-warning btn-block' : 'btn btn-dark btn-block'}
+              type='submit'
+            >
+              { editMode ? 'Guardar':'Agregar'}
+            </button>
+          </form>          
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
